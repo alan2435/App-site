d4396321220b550ff0ca67e781d76e35fee19c8a
alan2435 <123400308+alan2435@users.noreply.github.com>
2023-11-21 12:35:37 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, request
+# import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Setup app to use a sqlalchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sampleddb.db'
+db = SQLAlchemy(app)
+
+
+
+# Setup a simple table for database
+class Visitor(db.Model):
+    username = db.Column(db.String(100), primary_key = True)
+    numVisits = db.Column(db.Integer, default = 1)  
+
+    def __repr__(self):
+        return f"{self.username} - {self.numVisits}"
+    
+# Create tables in Database
+with app.app_context():
+    db.create_all()
 
 # Make a homepage
 @app.route('/')
@@ -17,8 +36,25 @@ def hello(name):
 def formDemo(name = None):
     if request.method == 'POST':
         name=request.form['name']
+        # Check if user is in the database
+        visitor = Visitor.query.get(name)
+        if visitor == None:
+            # Add Visitor to the database
+            visitor = Visitor(username = name)
+            db.session.add(visitor)
+        else:
+            visitor.numVisits +=1
+
+        db.session.commit()
+
     return render_template('form.html', name=name)
-    
+
+# Add in a page to view all visitors
+@app.route('/visitors')
+def visitors():
+    # Query the database to find all visitors
+    people = Visitor.query.all()
+    return render_template('visitors.html', people = people)
 
 
 # Add the option to run this file directly
